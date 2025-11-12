edeb89ca55e092fa092e8752c5d29aa47c6716bc
MsVoxxie <odysseylove222@gmail.com>
2025-11-12 07:01:20 -0500

    feat: Implement player playtime endpoint to retrieve total playtime by username and instance

---
+++ src/server/routes/redis.ts
@@ -0,0 +1,30 @@
+import redis from '../../loaders/database/redisLoader';
+import { getJson } from '../../utils/redisHelpers';
+import express from 'express';
+const router = express.Router();
+
+export const routeDescriptions = [
+	{
+		patch: '/data/playtime/:instance/:username',
+		method: 'GET',
+		description: 'Returns the total playtime for a specific player by username in a given instance.',
+	},
+];
+
+// Unified endpoint for player avatar
+router.get('/data/playtime/:instance/:username', async (req, res) => {
+	const { instance, username } = req.params;
+
+	await getJson(redis, `playerdata:${instance}:${username}`)
+		.then((playerData) => {
+			if (!playerData) {
+				return res.status(404).json({ error: 'Player data not found' });
+			}
+			return res.json(playerData);
+		})
+		.catch((error) => {
+			return res.status(500).json({ error: 'An error occurred while fetching player data.' });
+		});
+});
+
+export default router;
